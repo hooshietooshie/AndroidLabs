09e48373d5133285001c72c22663f219cb59ea60
HarshangShah <shah0300@algonquinlive.com>
2022-02-10 00:52:47 -0500

    Step_1

--- app/src/main/java/com/cst2335/shah0300/MainActivity.java
+++ app/src/main/java/com/cst2335/shah0300/MainActivity.java
@@ -1,14 +1,35 @@
 package com.cst2335.shah0300;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(
+                (v) -> {
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message), LENGTH_LONG).show();
+                }
+        );
+
+        Switch swh = findViewById(R.id.switch1);
+        swh.setOnCheckedChangeListener((cb, check) -> {
+            Snackbar.make(swh, getResources().getString(R.string.snack_message), Snackbar.LENGTH_LONG).setAction(getResources().getString(R.string.undo), click -> cb.setChecked(!check)).show();
+        });
+
     }
 }
