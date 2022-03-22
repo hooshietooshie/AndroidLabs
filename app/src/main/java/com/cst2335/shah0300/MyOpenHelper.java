0c37d56fe14c2e696ac6a8c519c543bf16c460e0
HarshangShah <shah0300@algonquinlive.com>
2022-03-22 00:22:42 -0400

    Finished Lab 5

--- app/src/main/java/com/cst2335/shah0300/MyOpenHelper.java
+++ app/src/main/java/com/cst2335/shah0300/MyOpenHelper.java
@@ -3,31 +3,32 @@ package com.cst2335.shah0300;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class MyOpenHelper extends SQLiteOpenHelper {
 
-    public static final String filename = "MyDatabase";
-    public static final int version = 1;
+    public static final String FILENAME = "MyDatabase";
+    public static final int VERSION = 2;
     public static final String TABLE_NAME = "MyData";
     public static final String COL_ID = "_id";
     public static final String COL_MESSAGE = "Message";
     public static final String COL_SEND_RECEIVE = "SendOrReceive";
-    public static final String COL_TIME_SENT = "TimeSent";
 
     public MyOpenHelper(Context context) {
-        super(context, filename, null, version);
+        super(context, FILENAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqlDB) {
         String result = String.format(" %s %s %s", "FirstString", "10", "10.0");
 
-        sqlDB.execSQL(String.format("Create table %s ( %s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s  INTEGER, %s TEXT );", TABLE_NAME, COL_ID, COL_MESSAGE, COL_SEND_RECEIVE, COL_TIME_SENT));
+        sqlDB.execSQL(String.format("Create table %s ( %s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s  INTEGER);", TABLE_NAME, COL_ID, COL_MESSAGE, COL_SEND_RECEIVE));
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqlDB, int oldVersion, int newVersion) {
         sqlDB.execSQL("Drop table if exists " + TABLE_NAME);
         this.onCreate(sqlDB);
+        Log.i(FILENAME, "DATABASE VERSION, oldVersion "+ oldVersion + ",newVersion " + newVersion);
     }
 }
