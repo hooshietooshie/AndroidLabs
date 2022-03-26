cb6fa29b5d952cb347787a17d5f5db5bfa6b92ca
HarshangShah <shah0300@algonquinlive.com>
2022-03-25 23:24:26 -0400

    Lab 06 initial commit

--- app/src/main/java/com/cst2335/shah0300/ChatRoomActivity.java
+++ app/src/main/java/com/cst2335/shah0300/ChatRoomActivity.java
@@ -1,10 +1,9 @@
 package com.cst2335.shah0300;
 
+import android.app.FragmentManager;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,11 +12,9 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,11 +26,13 @@ public class ChatRoomActivity extends AppCompatActivity {
     public static final String TAG = "ChatRoomActivity";
     MyOpenHelper myOpener;
     SQLiteDatabase theDatabase;
-    ListAdapter listAdapter;
     EditText message;
     Button btn_send;
     Button btn_receive;
     Adapter myAdapter;
+    ListView list;
+    FrameLayout frame;
+    Boolean tab_check = false;
     ArrayList<Message> message_arl = new ArrayList<>();
 
 
@@ -42,31 +41,27 @@ public class ChatRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
+        btn_send = findViewById(R.id.button);
+        btn_receive = findViewById(R.id.button4);
+        message = findViewById(R.id.editText7);
+        list = findViewById(R.id.Listview);
+        list.setAdapter(myAdapter = new Adapter());
+        //frame = findViewById(R.id.frame_layout);
+
+
+        tab_check = findViewById(R.id.frame_layout) != null;
+
+        DetailsFragment fragment = new DetailsFragment();
+        FragmentManager frag_mag = getFragmentManager();
+
         myOpener = new MyOpenHelper(this);
 
 
         theDatabase = myOpener.getWritableDatabase();
 
         Cursor results = theDatabase.rawQuery("Select * from " + MyOpenHelper.TABLE_NAME + ";", null);
+        print_Cursor(results, theDatabase.getVersion());
 
-        int idIndex = results.getColumnIndex(MyOpenHelper.COL_ID);
-        int messageIndex = results.getColumnIndex(MyOpenHelper.COL_MESSAGE);
-        int sendOrReceiveIndex = results.getColumnIndex( MyOpenHelper.COL_SEND_RECEIVE);
-        results.moveToPosition(-1);
-
-        while(results.moveToNext()){
-            long id = results.getLong(idIndex);
-            String message = results.getString(messageIndex);
-            boolean isFromSelf = results.getInt(sendOrReceiveIndex) == 1;
-            message_arl.list.add(new Message(message, isFromSelf, id));
-            myAdapter.notifyDataSetChanged();
-        }
-
-        btn_send = findViewById(R.id.button);
-        btn_receive = findViewById(R.id.button4);
-        message = findViewById(R.id.editText7);
-        ListView list = findViewById(R.id.Listview);
-        list.setAdapter(myAdapter = new Adapter());
 
 
 
@@ -75,7 +70,11 @@ public class ChatRoomActivity extends AppCompatActivity {
             Log.e(TAG, "Send button is clicked");
 
             if (!usr_message.isEmpty()) {
-                message_arl.add(new Message(usr_message, true, ));
+                ContentValues newRow = new ContentValues();
+                newRow.put(MyOpenHelper.COL_MESSAGE, usr_message);
+                newRow.put(MyOpenHelper.COL_SEND_RECEIVE, 1);
+                long id = theDatabase.insert(MyOpenHelper.TABLE_NAME, null, newRow);
+                message_arl.add(new Message(usr_message, true, id));
                 message.setText("");
                 myAdapter.notifyDataSetChanged();
             }
@@ -86,62 +85,43 @@ public class ChatRoomActivity extends AppCompatActivity {
             Log.e(TAG, "Receive button is clicked");
 
             if (!usr_message.isEmpty()) {
-                message_arl.add(new Message(usr_message, false));
+                ContentValues newRow = new ContentValues();
+                newRow.put(MyOpenHelper.COL_MESSAGE, usr_message);
+                newRow.put(MyOpenHelper.COL_SEND_RECEIVE, 0);
+                long id = theDatabase.insert(MyOpenHelper.TABLE_NAME, null, newRow);
+                message_arl.add(new Message(usr_message, false, id));
                 message.setText("");
                 myAdapter.notifyDataSetChanged();
             }
         });
 
-        list.setOnItemLongClickListener((adapterView, view, position, id) -> {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-            alertDialog.setTitle("Do you want to delete this?")
-                    .setMessage("The selected row is: " + position + "\n The database id is: " + id)
-                    .setPositiveButton("Yes", (v, args) -> {
-                        message_arl.remove(position);
-                        myAdapter.notifyDataSetChanged();
-                    })
-                    .setNegativeButton("No", (v, args) -> {
-                    })
-                    .create().show();
+        list.setOnItemClickListener((list1, view, position, id) -> {
+            if(tab_check != null) {
 
-            return true;
+            }
+
+
         });
 
 
-    }
+//        list.setOnItemLongClickListener((adapterView, view, position, id) -> {
+//            Message selected_message = message_arl.get(position);
+//            id = selected_message.getId();
+//            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+//            alertDialog.setTitle("Do you want to delete this?")
+//                    .setMessage("The selected row is: " + position + "\n The database id is: " + id)
+//                    .setPositiveButton("Yes", (v, args) -> {
+//                        message_arl.remove(position);
+//                        myAdapter.notifyDataSetChanged();
+//                    })
+//                    .setNegativeButton("No", (v, args) -> {
+//                    })
+//                    .create().show();
+//
+//            return true;
+//        });
 
-    private void utilMessage(boolean fromSelf) {
 
-        Message message1 = new Message(message.getText().toString(), fromSelf, id);
-        ContentValues newRow = new ContentValues();
-        newRow.put(MyOpenHelper.COL_MESSAGE, message1.getText());
-        newRow.put(MyOpenHelper.COL_SEND_RECEIVE, fromSelf ? 1 : 0);
-        long id = theDatabase.insert(MyOpenHelper.TABLE_NAME, null, newRow);
-        message1.setId(id);
-        Adapter.list.add(message1);
-        message.setText("");
-        myAdapter.notifyDataSetChanged();
-
-    }
-
-    private void createDialog(View view) {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setView(view);
-        alertDialog.setTitle("Do you want to delete this?");
-        alertDialog.setMessage("The selected row is: " + myAdapter.getCount());
-        alertDialog.setMessage("The database id is: ");
-        EditText input = new EditText(getApplicationContext());
-        alertDialog.setView(input);
-        alertDialog.setPositiveButton("remove", (click , v) -> {
-           String item = input.getText().toString();
-           myAdapter.notifyDataSetChanged();
-            Toast.makeText(getApplicationContext(), "Item deleted", Toast.LENGTH_SHORT).show();
-        });
-
-        alertDialog.create().show();
-        alertDialog.setNegativeButton("cancel", (click, v2) -> {
-            Log.i(TAG, "Cancelled");
-        });
     }
 
 
@@ -171,12 +151,12 @@ public class ChatRoomActivity extends AppCompatActivity {
             View send_view = inflater.inflate(R.layout.message_send, parent, false);
             TextView message_send = send_view.findViewById(R.id.send_text);
             message_send.setText(getItem(position).toString());
-            ImageView send_img = send_view.findViewById(R.id.send_Image);
+            //ImageView send_img = send_view.findViewById(R.id.send_Image);
 
             View receive_view = inflater.inflate(R.layout.message_receive, parent, false);
             TextView message_receive = receive_view.findViewById(R.id.receive_text);
             message_receive.setText(getItem(position).toString());
-            ImageView receive_img = receive_view.findViewById(R.id.receive_Image);
+            //ImageView receive_img = receive_view.findViewById(R.id.receive_Image);
 
             Message row = message_arl.get(position);
             if (row.sendOrReceive) {
@@ -214,4 +194,40 @@ public class ChatRoomActivity extends AppCompatActivity {
             this.id = id;
         }
     }
+
+    public Cursor print_Cursor(Cursor c, int version) {
+        int id_Index = c.getColumnIndex(MyOpenHelper.COL_ID);
+        int message_Index = c.getColumnIndex(MyOpenHelper.COL_MESSAGE);
+        int sendOrReceive_Index = c.getColumnIndex(MyOpenHelper.COL_SEND_RECEIVE);
+
+        int row_Count = c.getCount();
+        int column_Count = c.getColumnCount();
+
+        Log.i(TAG, "Database version: " + version);
+        Log.i(TAG, "Row count: " + row_Count);
+        Log.i(TAG, "Column count: " + column_Count);
+
+        Log.i(TAG, "Column Names: ");
+
+        for (int i = 0; i < column_Count; i++) {
+            String column_Name = c.getColumnName(i);
+            Log.i(TAG, "Column " + (i + 1) + ": " + column_Name);
+        }
+
+        while (c.moveToNext()) {
+            int id = c.getInt(id_Index);
+            String the_message = c.getString(message_Index);
+            int is_Sent = c.getInt(sendOrReceive_Index);
+
+            switch (is_Sent) {
+                case 1:
+                    message_arl.add(new Message(the_message, true, id));
+                    break;
+                case 2:
+                    message_arl.add(new Message(the_message, false, id));
+            }
+        }
+        return c;
+    }
+
 }
